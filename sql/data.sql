f1389dc56bae778640c2dc65b44bd600422bbd33
Michael Wilke <mwilke@enarchtech.com>
2022-09-22 22:03:44 -0500

    Update data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -1,4 +1,3 @@
-DROP TABLE IF EXISTS `test`;
 CREATE TABLE `test` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`identifier` VARCHAR(50) NOT NULL,
@@ -10,6 +9,7 @@ COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
 AUTO_INCREMENT=2;
 
--- The following updates tables that were not included in the original table (Support for those who already have the tables above)
-ALTER TABLE `test` ALTER COLUMN  `lastname` VARCHAR(50) NULL DEFAULT NULL; --Adding a new column
-ALTER TABLE `test` ADD `name` VARCHAR(50) NULL DEFAULT NULL; -- Updating an existing column
+ALTER TABLE `society_ledger` (
+	`taxRate` int(11) DEFAULT 0,
+	`repo` int(11) DEFAULT 0
+)
