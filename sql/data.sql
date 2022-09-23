4354fe783ffa030cb6c49a6a47eac450104d3401
Michael Wilke <mwilke@enarchtech.com>
2022-09-22 22:09:27 -0500

    Update data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -9,7 +9,5 @@ COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
 AUTO_INCREMENT=2;
 
-ALTER TABLE `society_ledger` (
-	`taxRate` int(11) DEFAULT 0,
-	`repo` int(11) DEFAULT 0
-)
+ALTER TABLE `society_ledger` ADD `taxRate` int(11) DEFAULT 0;
+ALTER TABLE `society_ledger` ADD `repo` int(11) DEFAULT 0;
